86a7c681c170ba86fa31d7d3cc09b2abe99e3350
Bikcode <bikcodeh@gmail.com>
2022-06-10 19:47:36 -0500

    add moongose and connect project to mongodb atlas

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
+require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql')
+const moongose = require('mongoose')
 const schema = require('./schema/schema')
 const schemaTest = require('./schema/types_schema')
+const { dbConnection } = require('./config');
 
 const app = express()
 
-app.use('/graphql',graphqlHTTP({
+
+app.use('/graphql', graphqlHTTP({
     graphiql: true,
     schema: schemaTest
 }))
+
+const connectDB = async() => {
+    await dbConnection()
+}
+
+connectDB()
+
 
 app.listen(4000, () => {
     console.log(`Server running at port 4000`)
